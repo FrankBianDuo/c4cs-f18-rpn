4e40d5d636b27b0e147bb4dfc9cfe441954585ed
bianduom <bianduom@umich.edu>
2018-10-26 11:18:15 -0400

    unit test first commit

---
+++ test_rpn.py
@@ -0,0 +1,14 @@
+def calculate(arg):
+	# stack for calculator
+	# tokenize input
+	# process tokens
+	pass
+
+def main():
+
+
+	import unittest
+	import rpn
+
+	class TestBasics(unittest, TestCase):
+		def test_add
