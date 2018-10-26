40128dd9a47313856b2c7ac71edea642c887aa20
bianduom <bianduom@umich.edu>
2018-10-26 11:30:16 -0400

    add tests for multiply and division

--- test_rpn.py
+++ test_rpn.py
@@ -12,3 +12,12 @@ class TestBasics(unittest, TestCase):
 	def test_sub(self):
 		result = rpn.calculate('4 3 -')
 		self.assertEqual(1, result)
+
+	def test_add(self):
+		result = rpn.calculate('2 2 *')
+		self.assertEqual(4, result)
+
+	def test_sub(self):
+		result = rpn.calculate('6 2 /')
+		self.assertEqual(3, result)
+
